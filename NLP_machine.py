217e887c24b0bdfa89137f21834f6aa00fb56715
Zach <zestela@gmail.com>
2017-10-24 09:04:34 -0700

    NLP ready for AWS

--- NLP_machine.py
+++ NLP_machine.py
@@ -23,11 +23,11 @@ class Model:
 
 class TopicModeler:
 
-    def __init__(self, tags_arcticles, n_top_words=5, n_topics=10, refit=True):
+    def __init__(self, tags_arcticles, n_top_words=10, n_topics=15, refit=False):
         self.refit = refit
         self.n_top_words = n_top_words
         self.n_topics = n_topics
-        self.text_ = islice(tags_arcticles, 200)
+        self.text_ = tags_arcticles
 
         self.vectorized = Model()
 
@@ -49,16 +49,23 @@ class TopicModeler:
                 weighting='tfidf',
                 normalize=True,
                 smooth_idf=True,
-                min_df=4,
+                min_df=2,
                 max_df=0.95,
                 max_n_terms=5000)
 
-            self.vectorized.doc_term_matrix = vectorizer.fit_transform((doc for doc in self.text_))
+            self.vectorized.doc_term_matrix = vectorizer.fit_transform((self.preprocess(doc)
+                                                                        for doc in self.text_))
 
             self.vectorized.feature_names = vectorizer.feature_names
             self.vectorized.vectorizer = vectorizer
 
             joblib.dump(self.vectorized, 'vectorizer.pkl')
+
+    def preprocess(self, doc):
+        flag, val = doc
+        self.vectorized.flag_index.append(flag)
+
+        return val
 
     def LSA(self):
         for topic in set(self.vectorized.flag_index):
@@ -66,17 +73,30 @@ class TopicModeler:
             self.topic_gen(topic)
 
     @timeit
-    def topic_gen(self, topic, model='nmf'):
-        dtm = self.vectorized.doc_term_matrix[np.array(self.vectorized.flag_index) == topic]
+    def topic_gen(self, topic, model='lda'):
+        if self.refit:
+            try:
+                os.remove('./lsa_lda{}.pkl'.format(topic))
+            except FileNotFoundError:
+                pass
+        try:
+            self.vectorized = joblib.load('./lsa_lda{}.pkl'.format(topic))
+            print('loaded lsa')
+        except Exception as e:
+            dtm = self.vectorized.doc_term_matrix[np.array(self.vectorized.flag_index) == topic]
 
-        if model == 'nmf':
-            model = NMF(n_components=self.n_topics).fit(dtm)
-        elif model == 'lda':
-            model = LatentDirichletAllocation(
-                n_components=self.n_topics, max_iter=10, learning_method='batch', learning_offset=50.)
+            if model == 'nmf':
+                model = NMF(n_components=self.n_topics).fit(dtm)
+            elif model == 'lda':
+                model = LatentDirichletAllocation(
+                    n_components=self.n_topics,
+                    max_iter=10,
+                    learning_method='batch',
+                    learning_offset=50.)
 
-        self.lsa_model = model.fit(dtm)
-        self.show(topic)
+            self.lsa_model = model.fit(dtm)
+
+            joblib.dump(self.lsa_model, './lsa_lda{}.pkl'.format(topic))
 
     def sentiment(self):
         print(TextBlob(' '.join(self.arcticles_tagged[tag])).sentiment, '\n')
@@ -95,12 +115,14 @@ class TopicModeler:
 
 def flags_articles_gen():
 
-    for i, _ in enumerate(mongo_driver.get_all('articles_clean')):
+    for i, _ in enumerate(mongo_driver.get_all('articles_cleaned')):
 
         yield _['flag'], _['article']
 
 
 #%%
+
+
 def run_vectorize():
     test = TopicModeler(flags_articles_gen())
     test.fit()
