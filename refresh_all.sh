9419647d9315fad2b23c8cb31eea424cb60b956a
Zach <zestela@gmail.com>
2017-10-26 17:03:25 -0700

    fix

--- refresh_all.sh
+++ refresh_all.sh
@@ -1,5 +1,5 @@
 pipreqs . --force
-python3 mongo_driver --kill articles
+python3 mongo_driver.py --kill articles
 python3 webcrawler.py
 python3 group_articles_by_flag.py
 python3 lemmatize_articles.py
