dfc7104cb51c128b6a91a0105cfc7becfa52ff34
Zach <zestela@gmail.com>
2017-10-26 17:02:59 -0700

    fix
    Former-commit-id: 5aba5fe0da03b3818afaf45abd150e49d3d91f0e

--- refresh_all.sh
+++ refresh_all.sh
@@ -1,5 +1,6 @@
+pipreqs . --force
 python3 mongo_driver --kill articles
-ptyhon3 webcrawler.py
+python3 webcrawler.py
 python3 group_articles_by_flag.py
 python3 lemmatize_articles.py
 python3 NLP_machine.py
