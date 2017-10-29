0708ac2707989202dc64e936e2083b11f4f4dfc2
Zach <zestela@gmail.com>
2017-10-29 16:19:09 -0700

    add folders to init

--- web/webserver_get.py
+++ web/webserver_get.py
@@ -5,8 +5,8 @@ from itertools import islice
 import requests
 
 import newspaper
-from ..helpers import timeit, LemmaTokenizer
-from ..web.plotter import plot
+from newscraper.helpers import timeit, LemmaTokenizer
+from newscraper.web.plotter import plot
 api = 'https://lbs45qdjea.execute-api.us-west-2.amazonaws.com/prod/newscraper'
 
 
