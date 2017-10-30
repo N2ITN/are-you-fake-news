b1e0b617f3307371b57b14eea77b5c4adb48e568
Zach <zestela@gmail.com>
2017-10-29 23:28:43 -0700

    webcrawler islice back to normal

--- get_process_data/webcrawler.py
+++ get_process_data/webcrawler.py
@@ -128,7 +128,7 @@ if __name__ == '__main__':
     news_sources = mongo_driver.get_all('all_sources')
     while True:
         try:
-            batch = itertools.islice(news_sources, 10)
+            batch = itertools.islice(news_sources, 90)
             threadpool()
 
         except StopIteration:
