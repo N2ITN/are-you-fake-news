c942c9a7f852366d40e9cae0056f7cbbea47d570
Zach <zestela@gmail.com>
2017-10-29 21:56:55 -0700

    refresh_all update

--- get_process_data/refresh_all.sh
+++ get_process_data/refresh_all.sh
@@ -1,4 +1,4 @@
-cd ..
+cd ~
 python3 newscraper/get_process_data/mongo_driver.py --kill articles
 python3 newscraper/get_process_data/webcrawler.py
 python3 newscraper/get_process_data/group_articles_by_flag.py
