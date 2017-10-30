0eda087036ba49307f1ee69020e7415887bcc68b
Zach <zestela@gmail.com>
2017-10-29 22:03:39 -0700

    refresh all update paths

--- get_process_data/refresh_all.sh
+++ get_process_data/refresh_all.sh
@@ -1,7 +1,7 @@
 cd ~
-python3 newscraper/get_process_data/mongo_driver.py --kill articles
-python3 newscraper/get_process_data/webcrawler.py
-python3 newscraper/get_process_data/group_articles_by_flag.py
-python3 newscraper/get_process_data/lemmatize_articles.py
-python3 newscraper/get_process_data/NLP_machine.py
+python3 mongo_driver.py --kill articles
+python3 webcrawler.py
+python3 group_articles_by_flag.py
+python3 lemmatize_articles.py
+python3 NLP_machine.py
 cd newscraper
