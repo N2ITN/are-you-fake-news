98e13d76264645446e295bfc42533528aaa95c33
Zach <zestela@gmail.com>
2017-10-30 21:22:09 -0700

    reset scale to high
    Former-commit-id: e97a5204bf822c50c6156f151e3a0e1888847bbf

--- web/plotter.py
+++ web/plotter.py
@@ -47,7 +47,7 @@ def plot(url, name_clean):
         y_pos = np.arange(len(y))
         plt.figure(figsize=(8, 8))
         g = sns.barplot(y=y_pos, x=x, palette=colors, orient='h', saturation=.9)
-        g.axes.set_xlim(0, 0.5)
+        g.axes.set_xlim(0, 0.4)
 
         plt.yticks(y_pos, y)
 
