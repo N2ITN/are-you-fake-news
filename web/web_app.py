af47fd7cd100effd3b09555c22c8ad3314e06424
Zach <zestela@gmail.com>
2017-10-31 00:53:13 -0700

    test enable cache

--- web/web_app.py
+++ web/web_app.py
@@ -11,7 +11,7 @@ from helpers import timeit
 DEBUG = True
 app = Flask(__name__)
 app.config.from_object(__name__)
-app.config["CACHE_TYPE"] = "null"
+# app.config["CACHE_TYPE"] = "null"
 app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
 
 
