f7b253a67f58518925520d8bc3dd4507684cb1b7
Zach <zestela@gmail.com>
2017-11-01 00:42:06 -0700

    fix web app loading

--- web/web_app.py
+++ web/web_app.py
@@ -1,4 +1,3 @@
-
 import os
 
 from time import sleep
@@ -37,7 +36,6 @@ def result():
         fact = './static/{}_{}.png'.format(name_clean, 'Accuracy')
         other = './static/{}_{}.png'.format(name_clean, 'Character')
 
-
     return render_template('index.html', pol=pol, fact=fact, other=other, value=pixel)
 
 
@@ -52,7 +50,7 @@ def result():
 #     r.headers["Expires"] = "0"
 #     r.headers['Cache-Control'] = 'public, max-age=0'
 #     return r
-    
+
 
 @app.route("/", methods=['GET', 'POST'])
 def hello():
@@ -74,42 +72,31 @@ def hello():
             def run_command():
                 return webserver_get.GetSite(url=name, name_clean=name_clean).run()
 
-            value = 'fakevalue'
             pixel = 'static/{}.png'.format('pixel11')
-            if not os.path.exists(value):
-                try:
-                    result = run_command()
-                    sleep(.5)
-                    if not result:
-                        flash('Not a real website.', 'error')
-                        oops = './static/oops.gif'          
-                        return render_template(
-                            'index.html', value=oops, pol=pixel, fact=pixel, other=pixel)
-                    else:
-                        pol = './static/{}_{}.png'.format(name_clean, 'Political')
-                        fact = './static/{}_{}.png'.format(name_clean, 'Accuracy')
-                        other = './static/{}_{}.png'.format(name_clean, 'Character')
-                        n_articles, polarity, subjectivity = result
-                        alert('Analysis based on {} most recent articles.'.format(n_articles), 'error')
-                        flash('positivity {}:'.format(polarity), 'error')
-                        flash('subjectivity {}:'.format(subjectivity), 'error')
-                finally:
 
-                    del form
-            else:
-                pass
+            result = run_command()
             sleep(.5)
-            #return render_template('index.html', pol=pol, fact=fact, other=other, value=pixel)
-            return redirect(url_for('result', results_form=value))
+            if not result:
+                flash('Not a real website.', 'error')
+                oops = './static/oops.gif'
+                return render_template('index.html', value=oops, pol=oops, fact=oops, other=oops)
+            else:
+                pol = './static/{}_{}.png'.format(name_clean, 'Political')
+                fact = './static/{}_{}.png'.format(name_clean, 'Accuracy')
+                other = './static/{}_{}.png'.format(name_clean, 'Character')
+                n_articles, polarity, subjectivity = result
+                alert('Analysis based on {} most recent articles.'.format(n_articles), 'error')
+                flash('positivity {}:'.format(polarity), 'error')
+                flash('subjectivity {}:'.format(subjectivity), 'error')
 
-            # Save the comment here.
+        sleep(.5)
+        #return render_template('index.html', pol=pol, fact=fact, other=other, value=pixel)
+        return redirect(url_for('result', results_form=form))
 
-        else:
-            flash('All the form fields are required. ')
+        # Save the comment here.
 
     return render_template('submit.html', form=form)
 
 
 if __name__ == '__main__':
     app.run(debug=False, threaded=True)
-
