05f1f211ad8411f1a6429b8e53409dd193de3513
Zach <zestela@gmail.com>
2017-10-31 22:46:38 -0700

    try cache function
    Former-commit-id: bfc37a98d593d7d8009eb229a60f953beee7cd3b

--- web/web_app.py
+++ web/web_app.py
@@ -24,6 +24,19 @@ def view_results():
     pass
 
 
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+
+
 @app.route("/", methods=['GET', 'POST'])
 def hello():
     form = ReusableForm(request.form)
