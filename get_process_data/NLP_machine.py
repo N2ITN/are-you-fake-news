eabe64c068c7f9f47f962fc8e8901185483bfad9
Zach <zestela@gmail.com>
2017-10-31 19:34:36 -0700

    denoise works
    Former-commit-id: c16da8138706100355d4b129cfc76b027e75ba0d

--- get_process_data/NLP_machine.py
+++ get_process_data/NLP_machine.py
@@ -40,12 +40,10 @@ class TopicModeler:
 
             # self.vectorized.feature_names = vectorizer.feature_names
             vectorizer = TfidfVectorizer(norm='l1', max_df=0.9, max_features=20000)
-            # max_features=10000,)
 
             self.doc_term_matrix = vectorizer.fit_transform((self.preprocess(doc) for doc in self.text_))
             self.vectorized.vectorizer = vectorizer
             print(dir(self.vectorized.vectorizer))
-            # print(vars(vectorizer))
 
             # open('keywords.txt', 'w').write(str(vectorizer.feature_names))
             joblib.dump(self.vectorized, 'vectorizer.pkl')
--- get_process_data/cosine_dist.py
+++ get_process_data/cosine_dist.py
@@ -51,14 +51,14 @@ class CosineCalcs:
 
     def distances(self):
         for v in CosineCalcs.vectors:
-            print(self.in_vect.shape)
-            print(self.in_vect.mean(axis=0).shape)
-            print(self.vec_from_model(v).shape)
-            print('wtf')
+            # print(self.in_vect.shape)
+            # print(self.in_vect.mean(axis=0).shape)
+            # print(self.vec_from_model(v).shape)
+            # print('wtf')
             # exit()
 
             dist = cosine_distances(self.vec_from_model(v), self.in_vect.mean(axis=0)).T
-            print(dist.shape)
+            # print(dist.shape)
             yield v, round(float(1 - dist), 3)
             # yield v, round(1. - float(cosine_distances(self.vec_from_model(v), self.in_vect)), 3) 
 
@@ -90,81 +90,82 @@ class Classify:
             return {}
 
 
-print(
-    orchestrate(('''Call it how it is
-Hendrix
-I promise, I swear, I swear
-You heard, spit it, yo
+if __name__ == '__main__':
+    print(
+        orchestrate(('''Call it how it is
+    Hendrix
+    I promise, I swear, I swear
+    You heard, spit it, yo
 
-Percocets, molly, Percocets
-Percocets, molly, Percocets
-Rep the set, gotta rep the set
-Chase a check, never chase a bitch
-Mask on, fuck it, mask off
-Mask on, fuck it, mask off
-Percocets, molly, Percocets
-Chase a check, never chase a bitch
-Don't chase no bitches
+    Percocets, molly, Percocets
+    Percocets, molly, Percocets
+    Rep the set, gotta rep the set
+    Chase a check, never chase a bitch
+    Mask on, fuck it, mask off
+    Mask on, fuck it, mask off
+    Percocets, molly, Percocets
+    Chase a check, never chase a bitch
+    Don't chase no bitches
 
-Two cups, toast up with the gang
-From food stamps to a whole 'nother domain
-Out the bottom, I'm the livin' proof (Super)
-Ain't compromising, half a million on the coupe
-Drug houses, lookin' like Peru
-Graduated, I was overdue
-Pink molly, I can barely move
-Ask about me, I'm gon' bust a move
-Rick James, thirty-three chains
-Ocean air, cruisin' Biscayne
-Top off, that’s a liability
-Hit the gas, boostin' my adrenaline
+    Two cups, toast up with the gang
+    From food stamps to a whole 'nother domain
+    Out the bottom, I'm the livin' proof (Super)
+    Ain't compromising, half a million on the coupe
+    Drug houses, lookin' like Peru
+    Graduated, I was overdue
+    Pink molly, I can barely move
+    Ask about me, I'm gon' bust a move
+    Rick James, thirty-three chains
+    Ocean air, cruisin' Biscayne
+    Top off, that’s a liability
+    Hit the gas, boostin' my adrenaline
 
-Percocets, molly, Percocets
-Percocets, molly, Percocets
-Rep the set, gotta rep the set
-Chase a check, never chase a bitch
-Mask on, fuck it, mask off
-Mask on, fuck it, mask off
-Percocets, molly, Percocets
-Chase a check, never chase a bitch
-Don't chase no bitches
+    Percocets, molly, Percocets
+    Percocets, molly, Percocets
+    Rep the set, gotta rep the set
+    Chase a check, never chase a bitch
+    Mask on, fuck it, mask off
+    Mask on, fuck it, mask off
+    Percocets, molly, Percocets
+    Chase a check, never chase a bitch
+    Don't chase no bitches
 
-[Verse 2]
-Four-door Maybach, I drive anything
-Buy my Range, make 'em go insane
-(Oh my Lord, praise him be)
-My guillotine, drank promethazine
-TEC and beams, go to those extremes
-Parliament, calamari Wednesday
-Parlay in Vegas, we was in attendance
-Before the business, Theodore lenses
-Theo-Dur prescriptions, focus on the missions
-Intermission, never take a break
-Switch states, touch down, foreign plates
-Ain't no way, ain't no fuckin' way
-We call the play, we didn't come to play
-Rob the bank, we gon' rob the game
-They gang, we gang
-But they are not the same (Freebandz)
+    [Verse 2]
+    Four-door Maybach, I drive anything
+    Buy my Range, make 'em go insane
+    (Oh my Lord, praise him be)
+    My guillotine, drank promethazine
+    TEC and beams, go to those extremes
+    Parliament, calamari Wednesday
+    Parlay in Vegas, we was in attendance
+    Before the business, Theodore lenses
+    Theo-Dur prescriptions, focus on the missions
+    Intermission, never take a break
+    Switch states, touch down, foreign plates
+    Ain't no way, ain't no fuckin' way
+    We call the play, we didn't come to play
+    Rob the bank, we gon' rob the game
+    They gang, we gang
+    But they are not the same (Freebandz)
 
 
-[Chorus]
-Percocets, molly, Percocets
-Percocets, molly, Percocets
-Rep the set, gotta rep the set
-Chase a check, never chase a bitch
-Mask on, fuck it, mask off
-Mask on, fuck it, mask off
-Percocets, molly, Percocets
-Chase a check, never chase a bitch
-Don't chase no bitches
+    [Chorus]
+    Percocets, molly, Percocets
+    Percocets, molly, Percocets
+    Rep the set, gotta rep the set
+    Chase a check, never chase a bitch
+    Mask on, fuck it, mask off
+    Mask on, fuck it, mask off
+    Percocets, molly, Percocets
+    Chase a check, never chase a bitch
+    Don't chase no bitches
 
-[Outro]
-Mask on, fuck it, mask off
-Mask on, fuck it, mask off
-Mask on, fuck it, mask off
-Gas gone, never nod off
-(Cold chills, prison cells)
-(Oh my Lord, praise him be)
+    [Outro]
+    Mask on, fuck it, mask off
+    Mask on, fuck it, mask off
+    Mask on, fuck it, mask off
+    Gas gone, never nod off
+    (Cold chills, prison cells)
+    (Oh my Lord, praise him be)
 
-''')))
+    ''')))
