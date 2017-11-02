80bbff23dd36568cd7c0ec442f1e768a6fd1bf52
Carrie0302 <clipsong@gmail.com>
2017-11-01 20:13:44 -0700

    colors in website and icons
    Former-commit-id: fd9968723e464bb6be37eac35f2c0e4ce2f765a3

--- web/plotter.py
+++ web/plotter.py
@@ -35,7 +35,7 @@ def plot(url, name_clean):
 
         return
 
-    sns.set(style='whitegrid', font_scale=1.7)
+    sns.set(style='whitegrid',  font='Tahoma', font_scale=1.7)
 
     def label_cleaner(y):
         key = {
@@ -74,8 +74,23 @@ def plot(url, name_clean):
         "unreliable": 0.013909999999999976,
         "veryhigh": 0.017959999999999973
     }
+    default_cp = ["#9b59b6", "#3498db", "#95a5a6", "#e74c3c", "#34495e", "#2ecc71"]
+    policic_colors = ["#9c3229", "#C8493A", "#D6837F", "#DCDDDD", "#98B5C6", "#6398C9", "#3F76BB"]
+    veracity_colors = ["#444784", "#2F7589", "#29A181", "#7CCB58"]
+    charachter_colors = ["#444784", "#7CCB58", "#3976C5", "#02B97C", "#C8493A"]
+
 
     def make_fig(x, y, cat, colors='coolwarm_r'):
+        color_p = default_cp
+        if cat == "Political":
+            color_p = policic_colors
+        elif cat == "Accuracy":
+            color_p = veracity_colors
+        elif cat == "Character":
+            color_p = charachter_colors
+
+
+
 
         y = list(label_cleaner(y))
 
@@ -84,7 +99,7 @@ def plot(url, name_clean):
         # x = np.square(np.asarray(x))
         x = np.asarray(x)
         print(dict(zip(y, x.round(4).astype(str))))
-        g = sns.barplot(y=y_pos, x=x, palette=colors, orient='h', saturation=.9)
+        g = sns.barplot(y=y_pos, x=x, palette=(sns.color_palette(color_p)), orient='h', saturation=.9)
         plt.yticks(y_pos, y)
         plt.title('{} - {}'.format(url, cat))
         plt.xlabel('Text similarity')
@@ -96,15 +111,17 @@ def plot(url, name_clean):
 
         plt.clf()
 
+
+
     get_spectrum(
         ['extremeright', 'right', 'right-center', 'center', 'left-center', 'left',
-         'extremeleft'], 'Political', 'coolwarm_r')
+         'extremeleft'], 'Political', 'policic_colors')
 
-    get_spectrum(['veryhigh', 'high', 'mixed', 'low', 'unreliable'], 'Accuracy', 'viridis')
+    get_spectrum(['veryhigh', 'high', 'mixed', 'low', 'unreliable'], 'Accuracy', 'veracity_colors')
     plt.close('all')
 
-    get_spectrum(['conspiracy', 'fakenews', 'propaganda', 'pro-science', 'hate'], 'Character', 'husl')
+    get_spectrum(['conspiracy', 'fakenews', 'propaganda', 'pro-science', 'hate'], 'Character', 'charachter_colors')
 
 
 if __name__ == '__main__':
-    plot('bbr', 'naturalnewscom')
+    plot(' Test CNN.com', 'cnncom')
