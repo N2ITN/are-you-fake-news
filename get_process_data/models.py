b66aad1dbbecf87fc7858755efb70e2abc30284f
Zach <zestela@gmail.com>
2017-11-04 20:42:54 -0700

    try separate vectors
    Former-commit-id: 1f7cc4a4c54e37fc673e2179913623d004935a39

--- get_process_data/models.py
+++ get_process_data/models.py
@@ -2,6 +2,5 @@ class Model:
 
     def __init__(self):
         self.doc_term_matrix = None
-        self.feature_names = None
-        self.flag_index = []
         self.vectorizer = None
+        self.nmf = None
