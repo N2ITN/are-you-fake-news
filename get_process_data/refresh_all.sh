9727466075f1411b406149991faf8e014a9c1140
Zach <zestela@gmail.com>
2017-11-13 06:05:27 +0000

    server side update
    Former-commit-id: 60d10729f7395e8936762720d72866617c8a9e71

--- get_process_data/refresh_all.sh
+++ get_process_data/refresh_all.sh
@@ -1,5 +1,6 @@
-
-# python3 mongo_driver.py --kill articles
+#python3 labels_MBFC.py
+#python3 join_source_lists.py
+python3 mongo_driver.py --kill articles
 python3 webcrawler.py
 python3 group_articles_by_flag.py
 python3 lemmatize_articles.py
