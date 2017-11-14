66180bae0f282dd1a824c4b64dcad6a5c2d3a7d7
Zach <zestela@gmail.com>
2017-11-14 03:32:48 +0000

    bash working update
    Former-commit-id: 55594bd6af8eaa0b7dfe21ed0dbc7802f9c40252

--- get_process_data/refresh_all.sh
+++ get_process_data/refresh_all.sh
@@ -1,8 +1,8 @@
 #python3 labels_MBFC.py
 #python3 join_source_lists.py
-python3 mongo_driver.py --kill articles
-python3 webcrawler.py
-python3 group_articles_by_flag.py
+#python3 mongo_driver.py --kill articles
+#python3 webcrawler.py
+#python3 group_articles_by_flag.py
 python3 lemmatize_articles.py
 python3 NLP_machine.py
 
