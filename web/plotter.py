034a78efbbd51897ce0069e7bee26331d23c0d69
Zach <zestela@gmail.com>
2017-11-13 17:03:53 -0800

    rm noise compensation
    Former-commit-id: 5ae4b51c5496cd2efa0adb4594a2cbc2a4f97f3b

--- web/plotter.py
+++ web/plotter.py
@@ -30,7 +30,7 @@ def plot(url, name_clean):
         for key in xy:
             if key in noise_factor:
                 # xy[key] = xy[key] * (noise_factor[key])
-                xy[key] -= noise_factor[key] * .9
+                # xy[key] -= noise_factor[key] * .9
 
                 pass
 
