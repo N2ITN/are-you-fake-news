3dc406fce4b0ee0876e353e24b176ad986daab90
Zach <zestela@gmail.com>
2017-12-05 13:57:21 -0800

    update for clarity

--- get_process_data/NLP_nn.py
+++ get_process_data/NLP_nn.py
@@ -18,19 +18,12 @@ class TopicModeler:
     def fit(self):
 
         tokenizer = Tokenizer(num_words=self.n_words)
-        #%% mode='tfidf')
 
         tokenizer.fit_on_texts([self.preprocess(doc) for doc in self.text_])
-        # tokenizer.texts_to_matrix(self.text_, mode='tfidf')
-        # print(sorted(tokenizer.word_counts.items(), key=lambda x: x[1]))
 
         pickle.dump(tokenizer, open('lsa_{}.pkl'.format(self.topic.replace(' ', '')), 'wb'))
 
     def preprocess(self, doc):
-
-        if self.topic == None:
-            self.topic = doc['flag']
-
         return ' '.join(doc['article'])
 
 
