f95070b9d9d7032fd734c97eb63130c69b7cc217
Zach <zestela@gmail.com>
2018-01-12 20:57:31 -0800

    add IP records blacklist

--- web/mongo_ip.py
+++ web/mongo_ip.py
@@ -40,6 +40,13 @@ def get_coords():
     }])
 
 
+def blacklist():
+    """ Delete my own IP from records to not bias maps """
+    my_ips = ['73.239.218.218', '71.212.99.244', '75.172.58.4', '97.113.121.210', '73.239.218.218']
+    for ip in my_ips:
+        db['ip_logs'].delete_many({'ip': ip})
+
+
 if __name__ == '__main__':
     import sys
     try:
